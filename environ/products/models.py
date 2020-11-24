3bf7d761ca89dfb72ed4a1cc2f9fbc4f3b8f1c11
Cassandra Talbot <40823509+CassandraTalbot32@users.noreply.github.com>
2020-11-24 12:37:43 +0000

    Update models.py

--- environ/products/models.py
+++ environ/products/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.urls import reverse
 
-# Create your models here.
+#attributes
 class Product(models.Model):
 	title		= models.CharField(max_length=120)
 	description = models.TextField(blank=True, null=True)
